8b563b0047d0c85d2ef0735f59322093c67b27dc
Hadrian Lau <118540129+udontur@users.noreply.github.com>
2025-02-01 20:35:49 +0800

    Uploaded configuration.nix from gaming pc.

---
+++ configuration.nix
@@ -0,0 +1,108 @@
+# UdonTur's NixOS Configuration File (Gnome 47) (please wait for hyprland)
+# Website: www.hadrianlau.com
+# GitHub: github.com/udontur
+# NixOS manual: https://nixos.org/manual/nixos/stable/
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Hyprland LETS GOOOOO
+  programs.hyprland = {
+    # Install the packages from nixpkgs
+    enable = true;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+  };
+
+  # Search package: $ nix search <PACKAGE NAME> or https://search.nixos.org/packages?channel=24.11&size=50&sort=relevance&type=packages&
+  # Make sure to check the options: https://search.nixos.org/options?channel=24.11&size=50&sort=relevance&type=packages
+  environment.systemPackages = with pkgs; [
+    neofetch
+    kitty
+    vscode
+    nixfmt-rfc-style
+    firefox
+    hyprland
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  #programs.mtr.enable = true;
+  #programs.gnupg.agent = {
+  #  enable = true;
+  #  enableSSHSupport = true;
+  #};
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  #services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  #networking.firewall.allowedTCPPorts = [ ... ];
+  #networking.firewall.allowedUDPPorts = [ ... ];
+  
+  # Or disable the firewall altogether.
+  #networking.firewall.enable = false;
+  
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "laptop-nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Hong_Kong";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_HK.UTF-8";
+
+  # Enable the X11 windowing system.
+  #services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  # Enable touchpad support (enabled default in most desktopManager).
+  #services.xserver.libinput.enable = true;
+  users.users.udontur = {
+    isNormalUser = true;
+    description = "Hadrian";
+    # Don't forget to set a password with ‘passwd’.
+    #passwd = "";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "24.11";
+  services.printing.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+}
